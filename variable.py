1ea16f287f0a6afd1375763fd52d7e65a805a80e
asd1025 <tmwrsts@daum.net>
2019-06-12 13:48:27 +0900

    Initial commit

---
+++ variable.py
@@ -0,0 +1,23 @@
+# 변수 이름은 문자, 숫자, _로 구성된다.
+import keyword
+
+friend = 1
+a = 10
+my_name = '안대혁'
+myName = '안대혁'
+_yourname = '둘리'
+member1 = '도우넛'
+
+# 에러
+# friend$ = 1
+# a1 = 10
+# 1abc = 1
+# def = 10
+
+print(keyword.kwlist)
+
+
+# 한글 이름의 변수도 사용이 가능하다
+가격1 = 1000
+print(가격1-500)
+
